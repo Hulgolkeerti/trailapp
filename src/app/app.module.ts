c2b75ad2a6b0b301365b3b40fa8568b07430a406
hulgolkeerti <hulgolkeerti99@gmail.com>
2020-03-12 12:23:41 +0530

    first attempt

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatInputModule} from '@angular/material/input';
+import {MatCardModule } from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import {MatButtonModule } from '@angular/material/button';
+import {HeaderComponent} from './header/header.component';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create.component';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
